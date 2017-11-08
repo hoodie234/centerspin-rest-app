db48f160c472b36e028d1648126b02569602fa92
Connor Hood <connorh@Connors-MacBook-Air.local>
2017-11-08 10:16:13 -0800

    Added package to hold ApplicationConfig class

---
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package org.netbeans.rest.application.config;
+import java.util.*;
+import javax.ws.rs.core.Application;
+
+
+@javax.ws.rs.ApplicationPath("centerspin")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.centerspin.rest.app.resources.ContentResource.class);
+        resources.add(com.centerspin.rest.app.resources.ScraperResource.class);
+        resources.add(com.centerspin.rest.app.resources.SubmitResource.class);
+    }
+
+}
