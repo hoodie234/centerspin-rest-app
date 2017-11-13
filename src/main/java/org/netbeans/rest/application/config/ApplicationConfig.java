73899e037033ea93a09234bc7618550ead0ac30d
Connor Hood <connorh@Connors-MacBook-Air.local>
2017-11-13 14:17:12 -0800

    Change to an auto-generated file that makes no sense

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -20,10 +20,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.centerspin.rest.app.resources.ContentResource.class);
-        resources.add(com.centerspin.rest.app.resources.PendingApprovalResource.class);
-        resources.add(com.centerspin.rest.app.resources.ScraperResource.class);
-        resources.add(com.centerspin.rest.app.resources.SubmitResource.class);
     }
 
 }
